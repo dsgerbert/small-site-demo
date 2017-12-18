7feeaf6281199a1c1ef61bb156765e1fabfe1787
Pavel <pavel@cubietruck>
2017-12-18 06:46:13 +0300

    Create cmd launcher

--- small-site-demo.cmd
+++ small-site-demo.cmd
@@ -1,9 +1,1 @@
-#!/bin/bash
-java -jar ./target/small-site-demo-0.0.4.jar \
--Dfile.encoding=UTF-8 \
---server.port=8080 \
---spring.profiles.active=dev \
---spring.mail.host=localhost \
---spring.mail.port=537 \
---spring.mail.username=mail \
---spring.mail.password=secret
+java -jar ./target/small-site-demo-0.0.4.jar -Dfile.encoding=UTF-8 --server.port=8080 --spring.profiles.active=dev --spring.mail.host=localhost --spring.mail.port=537 --spring.mail.username=mail --spring.mail.password=secret
